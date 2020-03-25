f29e3ff27548961d51b4b3cf96a7b269d6b667aa
mhmmtkkl <makoklu32@gmail.com>
2020-03-25 16:53:41 -0400

    project1

---
+++ src/main1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class main1 {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s1 = scanner.next();
+        s1 = "asda";
+        System.out.print(s1);
+        scanner.close();
+
+    }
+}
